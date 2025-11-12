9d50b1306835b5aa6dec214ac02981ff5af5b7ed
Charles Lisboa <charles.lisboa2@gmail.com>
2025-11-12 04:38:20 +0000

    chore: configurar build e deploy automático para produção

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,36 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "production", // garante minificação e otimização
   entry: "./src/assets/js/main.js",
   output: {
-    filename: "bundle.js",
+    filename: "assets/js/bundle.js", // organiza JS dentro da pasta assets
     path: path.resolve(__dirname, "dist"),
-    clean: true
+    clean: true, // limpa o dist antes de cada build
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[name][ext]",
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      minify: true
-    })
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        useShortDoctype: true,
+      },
+    }),
   ],
-  devServer: {
-    static: "./dist",
-    hot: true
-  }
 };
